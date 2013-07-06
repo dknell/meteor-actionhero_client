22ebc66e353e81cd13c1a2f5059fc15850abe9a3
David Knell <dk45@mac.com>
2013-07-06 08:20:51 -0400

    bumped version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
 	summary: "actionhero_client packaged for Meteor."
 });
 
-Npm.depends({ 'actionhero_client': '2.0.0' });
+Npm.depends({ 'actionhero_client': '2.0.x' });
 
 Package.on_use(function (api) {
 	api.add_files('actionheroclient.js', 'server');
